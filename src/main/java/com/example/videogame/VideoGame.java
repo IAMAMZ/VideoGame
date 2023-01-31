3467ad6028d312cef60538e1dcb07bf4205fc46a
Miran Qarachatani <miranararat722@gmail.com>
2023-01-30 20:04:25 -0500

    Add getter and setter for the videoGame class.

--- src/main/java/com/example/videogame/VideoGame.java
+++ src/main/java/com/example/videogame/VideoGame.java
@@ -1,4 +1,42 @@
 package com.example.videogame;
 
 public class VideoGame {
+
+    private String title;
+    private Integer releaseYear;
+    private Double price;
+
+    public VideoGame(String title,Integer releaseYear,Double price){
+        setTitle(title);
+        setReleaseYear(releaseYear);
+        setPrice(price);
+    }
+
+    public void setTitle(String title) {
+        if(title.length()==0) {
+            throw new IllegalArgumentException("Please Enter a valid title");
+        }
+        else{
+            this.title=title;
+        }
+    }
+
+    public void setReleaseYear(Integer releaseYear){
+        if(releaseYear<1920 || releaseYear>2023) {
+            throw new IllegalArgumentException("Please enter a valid year");
+        }
+        else{
+            this.releaseYear = releaseYear;
+        }
+    }
+
+    public void setPrice(Double price) {
+        if(price<1) {
+            throw new IllegalArgumentException("Please enter a valid price");
+        }
+        else{
+            this.price =price;
+        }
+    }
 }
+
